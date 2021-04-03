9d38f5afaf87a9e6263c159bd280fb41f8426623
Lunerio <lunerio@outlook.es>
2021-04-03 14:59:51 -0300

    title for type

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Edit lines in ssh config file
 include stdlib
-file_line { 'add_key':
+file_line { 'Declare identity file':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/holberton',
 }
 
-file_line { 'no_pass':
+file_line { 'Turn off passwd auth':
   path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
   match => '^#.PasswordAuthentication yes',

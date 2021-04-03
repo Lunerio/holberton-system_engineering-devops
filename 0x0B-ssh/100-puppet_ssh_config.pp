ce5f592f08976a88f9ec826ceee831d53c3239ac
Lunerio <lunerio@outlook.es>
2021-04-03 15:24:12 -0300

    asd

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
-# using Puppet to make changes to our configuration file
-file_line {'Turn off passwd auth':
-    ensure => 'present',
-    path   => '/etc/ssh/ssh_config',
-    line   => '    PasswordAuthentication no',
-    match  =>  'PasswordAuthentication yes',
+# sets up a client SSH configuration file so that we can connect to a server without using a password
+include stdlib
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/holberton',
+  replace => true,
 }
-file_line {'Declare identity file':
-    ensure => 'present',
-    path   => '/etc/ssh/ssh_config',
-    line   => '    IdentityFile ~/.ssh/holberton',
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }

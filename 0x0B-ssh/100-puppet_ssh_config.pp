a7379bc59480e339dc40566efe339b216640daa0
Lunerio <lunerio@outlook.es>
2021-04-03 15:02:58 -0300

    asd

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,7 +6,8 @@ file_line { 'Declare identity file':
 }
 
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'PasswordAuthentication no',
-  match => '^#.PasswordAuthentication yes',
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   => '^#.PasswordAuthentication yes',
+  replace => true,
 }

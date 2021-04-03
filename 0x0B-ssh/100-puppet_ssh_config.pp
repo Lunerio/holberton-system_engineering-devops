58e60347188b83a339304b61823f8515bd6b5348
Lunerio <lunerio@outlook.es>
2021-04-03 15:57:31 -0300

    just in case

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# using Puppet to make changes to ssh configuration file
+file_line {'Turn off passwd auth':
+    path  => '/etc/ssh/ssh_config',
+    line  => '    PasswordAuthentication no',
+    match =>  'PasswordAuthentication yes',
+}
+file_line {'Declare identity file':
+    path => '/etc/ssh/ssh_config',
+    line => '    IdentityFile ~/.ssh/holberton',
+}

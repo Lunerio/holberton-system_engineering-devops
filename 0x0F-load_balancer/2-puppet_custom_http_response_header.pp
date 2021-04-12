9a5ce4c2c73e71754c536a2babdcfc7267e27996
Lunerio <lunerio@outlook.es>
2021-04-12 12:19:51 -0300

    redo task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,34 @@
 # Puppet manifest to create server and add custom header
-exec { 'http header':
-  command  => 'sudo apt-get update -y;
-  sudo apt-get install nginx -y;
-  sudo sed -i "/server_name _/a add_header X-Served-By $hostname;" /etc/nginx/sites-available/default
-  sudo service nginx restart',
-  provider => shell,
+exec { 'update':
+  command  => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'redirect_line':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+}
+
+file_line { 'new_header':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }

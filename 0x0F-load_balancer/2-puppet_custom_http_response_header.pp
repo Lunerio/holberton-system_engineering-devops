4457ba321dc0907ff497fb3b4e3f0aca8ea97598
Lunerio <lunerio@outlook.es>
2021-04-12 18:03:27 -0300

    ya fue todo

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,7 @@
-# Puppet manifest to create server and add custom header
+# Manifest to create a server and set a new header
 exec { 'update':
-  command => 'apt-get update',
-  path    => '/usr/bin/:/bin/',
+  command => 'sudo apt-get update',
+  path    => ['/usr/bin', '/bin'],
 }
 
 package { 'nginx':
@@ -9,15 +9,16 @@ package { 'nginx':
   require => Exec['update'],
 }
 
-file_line { 'new_header':
-  ensure  => 'present',
+file_line { 'custom_header':
+  ensure  => present,
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $hostname;',
+  line    => "add_header X-Served-By ${hostname};",
   require => Package['nginx'],
 }
 
 exec { 'restart':
   command => 'sudo service nginx restart',
-  path    => '/usr/bin/:/usr/sbin/',
+  path    => ['/usr/bin', '/bin'],
+  require => File_line['custom_header'],
 }

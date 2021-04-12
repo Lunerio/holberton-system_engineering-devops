c5e4b239528724e0d4bb8b513547bbffe739cf85
Lunerio <lunerio@outlook.es>
2021-04-12 12:09:02 -0300

    test

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,8 @@
 # Puppet manifest to create server and add custom header
 exec { 'http header':
-	command  => 'sudo apt-get update -y;
-	sudo apt-get install nginx -y;
-	sudo sed -i "/server_name _/a add_header X-Served-By $hostname;" /etc/nginx/sites-available/default
-	sudo service nginx restart',
+  command  => 'sudo apt-get update -y;
+  sudo apt-get install nginx -y;
+  sudo sed -i "/server_name _/a add_header X-Served-By $hostname;" /etc/nginx/sites-available/default
+  sudo service nginx restart',
+  provider => shell,
 }

faa90fd90309493fee0f19d0ab75fe9d9a8c90ea
Lunerio <lunerio@outlook.es>
2021-05-03 17:13:30 -0300

    test 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     users = requests.get('https://jsonplaceholder.typicode.com/users').json()
 
     with open('todo_all_employees.json', mode='a') as file:
+        all_dict = {}
         for user in users:
             user_dict = {}
             tasks_list = []
@@ -21,5 +22,5 @@ if __name__ == '__main__':
                 new_dict["task"] = task.get('title')
                 new_dict["completed"] = task.get('completed')
                 tasks_list.append(new_dict)
-            user_dict[user.get('id')] = tasks_list
-        json.dump(user_dict, file)
+            all_dict[user.get('id')] = tasks_list    
+        json.dump(all_dict, file)

242a3959d8ed79f4c65280600942669441276d8c
lunerio <lunerio@pop-os.localdomain>
2021-05-19 12:36:46 -0300

    es un chiste

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# shall it be the comment here....
 exec { 'replace_line':
   path    => ['/bin', /usr/bin', /usr/sbin],
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart'

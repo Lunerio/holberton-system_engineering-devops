9bb3304f0c1f1a060c398b656bc4dfb9dbdabd58
lunerio <lunerio@pop-os.localdomain>
2021-05-19 12:31:58 -0300

    test

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 exec { 'replace_line':
   path    => '/bin',
-  command => 'sed -i s/phpp/php/ /var/www/html/wp-settings.php'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart'
 }
